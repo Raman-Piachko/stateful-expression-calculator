a4a4ecff900f4ce6840df5cc025c472942a07b81
Raman-Piachko <rpechko88@gmail.com>
2021-09-16 12:28:38 +0300

    delete initial commit

--- src/main/java/listeners/ListenerConstants.java
+++
@@ -1,9 +0,0 @@
-package listeners;
-
-public final class ListenerConstants {
-    private ListenerConstants() {
-    }
-
-    public static final String SESSION_ID_START = "Session with id = %s is started";
-    public static final String SESSION_ID_FINISHED = "Session with id = %s is finished";
-}
